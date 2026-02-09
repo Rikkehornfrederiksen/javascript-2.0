c74e063b10c86bf7c304151d5978e0675a450472
caroline-hue <caroline@iware.dk>
2026-02-09 10:43:02 +0100

    Update caroline.js

---
+++ caroline.js
@@ -0,0 +1,6 @@
+let burgermenu = document.querySelector(".burger")
+let dropdownmenu = document.querySelector(".dropdown")
+
+burgermenu.addEventListener("click", function() {
+    console.log("klik!");
+})
