6474e1fc6e794152a837b14f2d1b3c4eff96b0e0
caroline-hue <caroline@iware.dk>
2026-02-09 11:27:08 +0100

    d

--- caroline.js
+++ caroline.js
@@ -1,6 +1,34 @@
+//Array med objekter//
+let menuLinks = [
+    {navn: "Rikke", url: "rikke"},
+    {navn: "Louise", url: "louise"},
+    {navn: "Sarah", url: "sarah"}
+]
+
 let burgermenu = document.querySelector(".burger")
 let dropdownmenu = document.querySelector(".dropdown")
 
+//Loop gennem array//
+
+for(let i = 0; i < menuLinks.length; i++) {
+    // Hent det aktuelle objekt
+    let linkObjekt = menuLinks[i];
+    
+    // Lav et nyt <a> element
+    let linkElement = document.createElement("a");
+    
+    // Sæt href (url) på linket
+    linkElement.href = "#";
+    
+    // Sæt teksten i linket
+    linkElement.textContent = linkObjekt.navn;
+    
+    
+    // Tilføj linket til dropdown-menuen
+    dropdownmenu.appendChild(linkElement);
+}
+
+
 burgermenu.addEventListener("click", function() {
-    console.log("klik!");
+    dropdownmenu.classList.toggle('open');
 })
